7bb32a4202c5046c9a7b7f18845de16747caf9da
Tritiusss <94702697+Tritiusss@users.noreply.github.com>
2022-04-25 11:23:00 -0400

    Delete config directory

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-let db = module.exports = new sqlite3.Database('./db/mydatabase.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.log(err.message);
-    }else {
-        console.log('Connected to Database');
-    }
-});
